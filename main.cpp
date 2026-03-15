810fba5cf99829f9b0de738104393eb5d7f25f5e
Cooper Huntington-Bugg <cooperehuntington@gmail.com>
2026-03-15 11:38:49 -0500

    Added skeleton of main.cpp, will be adding TODO's and input's next

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+TODO:
+- Run simple gravity and force calculations
+- Input physical parameters such as mass or radius
+- View computed results such as gravitational force or weight
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout <<"<=> Try To Orbit: Mission Physics Console <=>\n"
+        "1. Compute weight on Moon\n"
+        "2. Compute gravitational force\n"
+        "3. Compute surface gravity\n"
+        "4. Exit\n"
+        "Enter your choice:";
+        
+        
+        
+        return 0;
+}
